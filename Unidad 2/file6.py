ba87ff6e50d6d932cac87203eb4a1a4715f119b3
Andan <141715477+AdanCC21@users.noreply.github.com>
2024-03-12 08:25:27 -0700

    Orden

--- Unidad 2/file6.py
+++ Unidad 2/file6.py
@@ -1,5 +1,24 @@
 """
     Adan Gonzalez Ceseña
     Gp 941
-    
+    12/03/2024
 """
+#For iterador in iterable
+for c in "hola":
+    print(c,end=" ")
+#sep
+print(10,20,30, sep=",")
+
+#{}
+print(f'{10+5}+{20}+{30}',90,sep=", ")
+
+#listas aunque no las vamos a usar
+total=0
+for numero in [2,-3,0,17,9]:
+    total=total+numero
+print(total)
+
+#Range
+for contador in range(10):
+    print(contador, end=" ")
+
