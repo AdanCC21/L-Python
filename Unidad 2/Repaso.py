4b416384c58be83b59f27b0cc2f6c4b61d6e4b89
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2024-04-08 21:53:54 -0700

    a

---
+++ Unidad 2/Repaso.py
@@ -0,0 +1,27 @@
+# print("ingrese una cantidad",end=("\n"))
+# c=int(input())
+
+# for i in range(c,10,22):
+#     print(i)
+
+"""
+Range(Final)
+Range(Inicio, Final)
+Range(Inicio, Final, pasos)
+"""
+
+# print(f'{10:<30}+{c:>10}')
+
+# sum=0
+# lis=[1,2,5,4]
+# for i in lis:
+#     sum=sum+i
+#     print(sum)
+
+dec=10.5
+dec2=10.5
+dec3=dec+dec2
+print(f'Decimal : {dec3:.10f}')#cantidad de decimales
+
+
+
