1fec4116f43225fbe7794672156e0cbeac7f412b
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2024-06-01 17:49:11 -0700

    HomeWork updated

---
+++ Ejercicio_listas/Eja.py
@@ -0,0 +1,14 @@
+import random
+
+def generaLista():
+    lista=[]
+    for i in range(5):
+        lista.append(random.randint(1,28))
+    lista.sort()
+    return lista
+
+list=generaLista()
+print(list)
+
+fa = open("Ejercicio_Listas/archivo1.txt","w")
+fa.write(str(list))
